6db91cd6b3118e8549fa95d0afb1f474a4cd68b5
Beardo Moore <beardo@adobe.com>
2012-03-22 09:38:59 -0600

    Update to Datatables 1.9

--- lib/jquery/datatables/rails/version.rb
+++ lib/jquery/datatables/rails/version.rb
@@ -1,7 +1,7 @@
 module Jquery
   module Datatables
     module Rails
-      VERSION = "0.1.0"
+      VERSION = "1.9.0" # align the version number of the gem to the version number of DataTables include
     end
   end
 end
